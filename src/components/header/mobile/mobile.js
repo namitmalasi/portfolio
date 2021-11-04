dfb71a5f3de55ffcdc4a93c3c63c66745c10ba0a
Namit Malasi <namitmalasi@gmail.com>
2021-11-04 20:14:14 +0530

    created the sidebar component

--- src/components/header/mobile/mobile.js
+++ src/components/header/mobile/mobile.js
@@ -1,7 +1,42 @@
 import React from "react";
+import close from "./close.png";
+import project from "../web/project.png";
+import contact from "../web/user.png";
+import skills from "../web/skills.png";
+import "./mobile.css";
 
-function Mobile() {
-  return <div>This is Mobile</div>;
+function Mobile({ isOpen, setIsOpen }) {
+  return (
+    <div className="mobile">
+      <div className="close-icon" onClick={() => setIsOpen(!isOpen)}>
+        <img src={close} alt="images"></img>
+      </div>
+
+      <div className="mobile-options">
+        <div className="mobile-option">
+          <a href="#project">
+            <img className="option-icon" src={project} alt="images"></img>
+            Projects
+          </a>
+        </div>
+
+        <div className="mobile-option">
+          <a href="#skills">
+            <img className="option-icon" src={skills} alt="images"></img>
+            Skills
+          </a>
+        </div>
+
+        <div className="mobile-option">
+          {" "}
+          <a href="#contact">
+            <img className="option-icon" src={contact} alt="images"></img>
+            Contact{" "}
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Mobile;
